c53e3e0791557ffdc56ebff99e46467a204fb9a3
Arturo-J-Alva <arturo.j.a.m@outlook.com>
2020-10-12 13:26:01 -0500

    push

--- .next/static/liJPIIj7DtqLPWsFvJuXW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f4f567df00ce0843964e.js"],"/inicio":["static\u002Fchunks\u002Fpages\u002Finicio-f0105ab6be8376633c7a.js"],sortedPages:["\u002F_app","\u002F_error","\u002Finicio"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
